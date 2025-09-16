1ec1467f31605b2bd343648f81b6e4d65393e027
RobinS55 <scholte.robin@gmail.com>
2025-09-16 16:50:58 +0200

    Update Room.jsx

--- src/Room.jsx
+++ src/Room.jsx
@@ -1,42 +1,42 @@
 import React from "react";
 
-export default function Room({ room, revealed, playerPos, offset }) {
+export default function Room({ room, revealed, playerPos }) {
   const TILE_SIZE = 30;
 
   return (
-    <>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${room.width}, ${TILE_SIZE}px)`,
+        gridTemplateRows: `repeat(${room.height}, ${TILE_SIZE}px)`
+      }}
+    >
       {revealed &&
         room.tiles.map((row, y) =>
           row.map((tile, x) => {
-            const globalX = x + offset.x;
-            const globalY = y + offset.y;
             let color = tile === "wall" ? "#222" : "#ccc";
 
             for (const dir of ["left", "right", "top", "bottom"]) {
               const exit = room.exits[dir];
-              if (exit && exit.x === x && exit.y === y) {
-                color = "#0f0";
-              }
+              if (exit && exit.x === x && exit.y === y) color = "#0f0";
             }
 
-            if (playerPos && playerPos.x === globalX && playerPos.y === globalY) {
-              color = "red";
-            }
+            if (playerPos && playerPos.x === x && playerPos.y === y) color = "red";
 
             return (
               <div
-                key={`${globalX},${globalY}`}
+                key={`${x},${y}`}
                 style={{
                   width: TILE_SIZE,
                   height: TILE_SIZE,
                   background: color,
-                  boxSizing: "border-box",
-                  border: "1px solid #555"
+                  border: "1px solid #555",
+                  boxSizing: "border-box"
                 }}
               />
             );
           })
         )}
-    </>
+    </div>
   );
 }
