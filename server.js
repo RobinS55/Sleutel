0a499af9c6747d5afe6c7f2f82053b97f1686e10
RobinS55 <scholte.robin@gmail.com>
2025-09-30 11:56:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 10000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "dist")));
 
